220fd29dfdd2f61e88f152833d38d725edb88d17
omaraalsaied <omar2181998@yahoo.com>
2024-01-20 01:52:48 +0200

    Auth using passport

---
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $valid_data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create($valid_data);
+        $token = $user->createToken('Personal Access Token')->accessToken;
+        return response()->json(['user' => $user, 'token' => $token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = User::find(Auth::user()->id);
+
+            $user_token['token'] = $user->createToken('appToken')->accessToken;
+
+            return response()->json([
+                'success' => true,
+                'token' => $user_token,
+                'user' => $user,
+            ], 200);
+        }
+        return response()->json(['error' => 'Unauthourized'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        if (Auth::user()) {
+            $request->user()->token()->revoke();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Logged out successfully',
+            ], 200);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["namespace"=> "Api","prefix"=> "v1"], function () {
+    Route::post('register', [AuthenticationController::class,'register']);
+    Route::post('login', [AuthenticationController::class,'login']);
+    Route::post('logout', [AuthenticationController::class,'logout'])->middleware('auth:api');
 });
